f07b5f82a7e0681c1c089be21913b5657de56bf2
Эльдияр Эмилов <emilov2501@gmail.com>
2020-05-23 17:23:17 +0600

    fix lint

--- src/shims-tsx.d.ts
+++ src/shims-tsx.d.ts
@@ -3,9 +3,12 @@ import Vue, { VNode } from "vue";
 declare global {
   namespace JSX {
     // tslint:disable no-empty-interface
+    // eslint-disable-next-line @typescript-eslint/interface-name-prefix
     interface Element extends VNode {}
     // tslint:disable no-empty-interface
+    // eslint-disable-next-line @typescript-eslint/interface-name-prefix
     interface ElementClass extends Vue {}
+    // eslint-disable-next-line @typescript-eslint/interface-name-prefix
     interface IntrinsicElements {
       [elem: string]: any;
     }
